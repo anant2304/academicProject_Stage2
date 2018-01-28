ae4182c42341dc401088adcf72b082848f61367e
AngryMajor <peterfmajor@gmail.com>
2018-01-28 15:00:34 +0000

    AutoCompleat Partly Finished
    Auto compleate can find the comands that contain the string you have entered by log them rather then displaying them

--- src/Sigurd/InputPannel/CommandPanel.java
+++ src/Sigurd/InputPannel/CommandPanel.java
@@ -6,15 +6,18 @@ import javax.swing.event.DocumentListener;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 public class CommandPanel {
 
 	public JPanel panel;
 	JTextField commandLine;
 	JButton enterButton;
+	Commands commands;
 	
 	public CommandPanel() {
 		panel = new JPanel();
+		commands = new Commands();
 		
 		AddCommandLine();
 		AddEnterButton();
@@ -22,16 +25,52 @@ public class CommandPanel {
 	
 	void AddCommandLine() {
 		commandLine = new JTextField(20);
+		
+		//action listener for when user presses return key
+		commandLine.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				ExicuteCommand(commandLine.getText());
+			}
+		});
+		
+		//adding listener for when someone types something into the text box
 		commandLine.getDocument().addDocumentListener(new DocumentListener() {
-			public void changedUpdate(DocumentEvent e) {}
+			public void changedUpdate(DocumentEvent e) {
+			}
+			
 			public void removeUpdate(DocumentEvent e) {
 				FindPartialMatch(commandLine.getText());
 			}
+			
 			public void insertUpdate(DocumentEvent e) {
 				FindPartialMatch(commandLine.getText());
 			}
 		});
+		
 		panel.add(commandLine);
+	}
+	
+	void AddEnterButton() {
+		enterButton = new JButton("Enter");
+		
+		enterButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ExicuteCommand(commandLine.getText());
+			}
+		});
+		
+		panel.add(enterButton);
+	}
+	
+	void FindPartialMatch(String part) {
+		//check for enmpty string as this would return the full commmand list
+		if(part.length() == 0) return;
+		
+		LinkedList<String> list = commands.FindCommands(part);
+		for(String s : list){
+			System.out.println(s);//for debug reasons
+			//TODO : put this on screen 
+		}
 	}
 	
 	public void ExicuteCommand(String command) {
@@ -39,26 +78,11 @@ public class CommandPanel {
 		default :
 			System.out.println("Command Not Recodnised");
 		} 
-	}
-	
-	void FindPartialMatch(String s) {
-		
-	}
-	
-	void AddEnterButton() {
-		enterButton = new JButton("Enter");
-		enterButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ExicuteCommand(commandLine.getText());;
-				commandLine.setText("");
-			}
-		});
-		panel.add(enterButton);
+		commandLine.setText("");
 	}
 	
 	public static void main(String[] args) {
 		CommandPanel cpannel = new CommandPanel();
-		
 		
 		JFrame frame = new JFrame();
 		frame.add(cpannel.panel);
