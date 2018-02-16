bd8301b1e98db9a67472dc17539af94663bbabc6
anant2304 <anantshawyo@gmail.com>
2018-02-16 23:20:29 +0000

    Update DisplayPanel.java
    Aligning text centrally in the display and adding gray color to the background of the display panel.

--- src/Sigurd/DisplayPanel.java
+++ src/Sigurd/DisplayPanel.java
@@ -1,9 +1,16 @@
 package Sigurd;
 
+import java.util.*;
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
-
+import javax.swing.*;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.text.*;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
+import javax.swing.text.Style;
 /**
  * Panel that displays inputs back to the user.
  * @author Anant Shaw
@@ -14,18 +21,24 @@ import java.awt.*;
 @SuppressWarnings("serial")
 public class DisplayPanel extends JPanel //class DisplayPanel acts a panel itself
 {
-    
-    JTextArea text1=new JTextArea(); //using JTextArea for the inputs to be scrollable
+    JTextPane tpane=new JTextPane();//using JTextArea for the inputs to be scrollable
     JScrollPane pane=new JScrollPane();
+    SimpleAttributeSet set=new SimpleAttributeSet();
+    StyledDocument text1=tpane.getStyledDocument();
     public DisplayPanel() //constructor to set the display panel
     {
         this.setLayout(new BorderLayout(10,10)); 
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
-        text1.setEditable(false);
-        text1.setColumns(30);
-        pane=new JScrollPane(text1); //to make the panel scrollable
-        add(pane); //adding the Scrollpane to the frame
-        sendMessage("   HELLO!   ");
+        this.setBackground(Color.RED);
+        tpane.setEditable(false);
+        StyleConstants.setAlignment(set, StyleConstants.ALIGN_CENTER);
+        tpane.setParagraphAttributes(set, true);
+        tpane.setOpaque(true);
+        tpane.setBackground(Color.gray);
+        pane=new JScrollPane(tpane); 
+        pane.setPreferredSize( new Dimension(363,632));
+        add(pane);
+        sendMessage("GAME BEGINS");
     }
     public static void main(String args[]) throws IOException //main function which takes the input string and sends it to the sendMessage function to be displayed in the panel, for testing
     {
@@ -37,6 +50,7 @@ public class DisplayPanel extends JPanel //class DisplayPanel acts a panel itsel
             panel.sendMessage(st); //calling the sendMessage function
         }
         JFrame frame=new JFrame(); //creating a new JFrame for testing 
+        frame.setBackground(Color.red);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
@@ -44,7 +58,14 @@ public class DisplayPanel extends JPanel //class DisplayPanel acts a panel itsel
     }
     public void sendMessage(String str) //function adds the string to the panel
     {
-        text1.append("  " + str + System.getProperty("line.separator")); // adding the list contents to the JTextArea
+        try
+        {
+            text1.insertString(text1.getLength(), str+"\n\n", null);
+        }
+        catch(Exception exe) 
+        { 
+            System.out.println(exe); 
+        }
     }
-    
 }
+
