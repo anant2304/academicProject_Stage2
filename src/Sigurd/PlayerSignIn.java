03ab4889d974fb65fbaccd8bd0588a2b2d0e8ab8
anant2304 <anantshawyo@gmail.com>
2018-02-23 14:48:32 +0000

    updated PlayerSignIn class

--- src/Sigurd/PlayerSignIn.java
+++ src/Sigurd/PlayerSignIn.java
@@ -1,124 +1,146 @@
 package Sigurd;
 import java.awt.*;
 import java.util.Scanner;
- 
+
+import Sigurd.BoardObjects.PlayerObject;
+
+import java.util.LinkedList;
+
 /*  Class PlayerSignIn  */
 class PlayerSignIn
 {
-	linkedList list = new linkedList();
-	protected String name;
+    LinkedList<PlayerObject> list = new LinkedList<PlayerObject>();
+    LinkedList<String> nameList=new LinkedList<String>();
+    protected String name;
     protected PlayerSignIn link;
-    //static CommandPanel command;
-	//public static DisplayPanel display;
- 
-    /*  Constructor  */
+    public int numPlayers;
+    boolean isnumPlayersSet;
+    CommandPanel command;
+    DisplayPanel display;
+    
     public PlayerSignIn()
     {
+        command=Game.GetCommand();
+        display= Game.GetDisplay();
         link=null;
         name="";
-        //command = new CommandPanel();
-		//display = new DisplayPanel();
-    }    
-    /*  Constructor  */
+        isnumPlayersSet=false;
+    }
     public PlayerSignIn(String n,PlayerSignIn l)
     {
         name=n;
         link=l;
-    }    
-    /*  Function to set link to next PlayerSignIn node */
+    }
+    /*Function to set link to next PlayerSignIn node*/
     public void setLink(PlayerSignIn n)
     {
         link = n;
-    }    
-    /*  Function to set data to current PlayerSignIn  node*/
+    }
+    /*Function to set data to current PlayerSignIn node*/
     public void setData(String n)
     {
         name=n;
-    }    
-    /*Function to get link to next node */
+    }
+    
+    public void add(PlayerObject e)
+    {
+        list.addLast(e);
+    }
+    
+    /*Function to get link to next node*/
     public PlayerSignIn getLink()
     {
         return link;
-    }    
-    /*  Function to get data from current PlayerSignIn node */
+    }
+    /*Function to get data from current PlayerSignIn node*/
     public String getName()
     {
         return name;
     }
-    
-    
-    
-}
-
- 
-/* Class linkedList */
-class linkedList
-{
-    protected PlayerSignIn start;
-    protected PlayerSignIn end;
-    public int size;
- 
-    /* Constructor */
-    public void display()
+    public void askPlayerNames()
     {
-        System.out.print("\nCircular Singly Linked List = ");
-        PlayerSignIn ptr = start;
-        if (size == 0) 
+        Game.GetDisplay().sendMessage("Enter the number of players\n");
+        if(isnumPlayersSet==false)
         {
-            System.out.print("empty\n");
-            return;
+            
         }
-        if (start.getLink() == start) 
-        {
-            System.out.print(start.getName()+ "->"+ptr.getName()+ "\n");
-            return;
-        }
-        System.out.print(start.getName()+ "->");
-        ptr = start.getLink();
-        while (ptr.getLink() != start) 
-        {
-            System.out.print(ptr.getName()+ "->");
-            ptr = ptr.getLink();
-        }
-        System.out.print(ptr.getName()+ "->");
-        ptr = ptr.getLink();
-        System.out.print(ptr.getName()+ "\n");
-    }
-    public linkedList()
-    {
-        start=null;
-        end=null;
-        size = 0;
-    }
-    /* Function to check if list is empty */
-    public boolean isEmpty()
-    {
-        return start == null;
-    }
-    /* Function to get size of the list */
-    public int getSize()
-    {
-        return size;
     }
     
-    
-    /* Function to insert element*/
-    public void insert(String player)
+    public void takeInput(String input)
     {
-        PlayerSignIn nptr=new PlayerSignIn(player,null);    
-        nptr.setLink(start);
-        if(start==null)
-        {            
-            start=nptr;
-            nptr.setLink(start);
-            end=start;            
+        if(isnumPlayersSet==false)
+        {
+            if(Character.isDigit(input.charAt(0)))
+            {
+                numPlayers=Integer.parseInt(input);
+                isnumPlayersSet=true;
+                Game.GetDisplay().sendMessage("Hello\n");
+            }
         }
         else
+            if(isnumPlayersSet==false && !Character.isDigit(input.charAt(0)))
+            {
+                Game.GetDisplay().sendMessage("Wrong Input\n Enter the number of players\n");
+            }
+            else
+                if(isnumPlayersSet==true && !Character.isDigit(input.charAt(0)) && !input.equalsIgnoreCase("Done"))
+                {
+                    nameList.addLast(input);
+                }
+                else
+                    if(input.equalsIgnoreCase("Done"))
+                    {
+                        if(nameList.size()==numPlayers)
+                        {
+                            Game.StartGame();
+                        }
+                        else
+                        {
+                            Game.GetDisplay().sendMessage("Wrong number of players inputted\n");
+                        }
+                    }
+        for(int i=0;i<nameList.size();i++)
         {
-            end.setLink(nptr);
-            end = nptr;            
+            Game.GetDisplay().sendMessage(nameList.get(i));
         }
-        size++ ;
     }
     
-    }
+    public static void main(String[] args)
+    {
+        
+        
+    				
+    				PlayerSignIn obj=new PlayerSignIn();
+    				Scanner scan = new Scanner(System.in);
+        System.out.println("Circular Singly Linked List Test\n");
+        char ch;
+        do
+        {
+            System.out.println("\nCircular Singly Linked List Operations\n");
+            System.out.println("1. insert");
+            System.out.println("5. check empty");
+            System.out.println("6. get size");
+            int choice = scan.nextInt();
+            switch (choice)
+            {
+                case 1 :
+                    System.out.println("Enter string element to insert");
+                    obj.name="Peter";
+                    obj.list.add(new PlayerObject(Coordinates.UP, Color.BLACK, "Peter", "Adrain"));
+                    break;
+                case 5 :
+                    System.out.println("Empty status = "+ obj.list.isEmpty());
+                    break;
+                case 6 :
+                    System.out.println("Size = "+ obj.list.size() +" \n");
+                    break;
+                default :
+                    System.out.println("Wrong Entry \n ");
+                    break;
+            }
+            System.out.println(obj.getName());
+            System.out.println("\nDo you want to continue (Type y or n) \n");
+            ch = scan.next().charAt(0);
+        } while (ch == 'Y'|| ch == 'y');
+    } 
+}
