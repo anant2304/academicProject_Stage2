cc2e095c2364205cfaca77734f62f8f01ba3a67e
Angrymajor <peterfmajor@gmail.com>
2018-04-16 12:31:49 +0100

    added card matris, log parser and question storage classes

--- COMP20050SigurdBot/src/bots/Sigurd.java
+++ COMP20050SigurdBot/src/bots/Sigurd.java
@@ -2,6 +2,9 @@ package bots;
 
 import java.util.*;
 
+import bots.CardMatrix.CardRow;
+import bots.CardMatrix.PlayerColum;
+import bots.CardMatrix.Position;
 import gameengine.*;
 import gameengine.Map;
 
@@ -210,6 +213,7 @@ public class Sigurd implements BotAPI {
         }
     }
 
+    
     class CardAgent {
 
         public CardAgent() {
@@ -221,4 +225,167 @@ public class Sigurd implements BotAPI {
         }
 
     }
+
+    class CardMatrix{
+        java.util.Map<String,CardRow> cardRow;
+        java.util.Map<Integer,PlayerColum> playerCol;
+        
+     	CardMatrix(Collection<String> cards, int numPlayers){
+     		cardRow = new HashMap<String, CardRow>();
+     		playerCol = new HashMap<Integer,PlayerColum>();
+     		
+     		for(String s : cards)
+     			cardRow.put(s, new CardRow(s));
+     		
+    		for(int i = 0; i < numPlayers; i++) {
+    			playerCol.put(i, new PlayerColum(i));
+    			PlayerColum tempPlay = playerCol.get(i);
+    			
+    			for(String s : cards) {
+        			CardRow tempCard = cardRow.get(s);
+        			
+        			Position tempPos = new Position(tempPlay.index,tempCard.name);
+        			tempCard.players.add(tempPos);
+        			tempPlay.cards.add(tempPos);
+    			}
+    		}
+    	}
+    	
+    	void CardFound(int p, String c) {
+    		cardRow.get(c).crossoutAllBarOne(p);
+    	}
+    	
+    	void PlayerDoseNotHave(int p, String c) {
+    		cardRow.get(c).crossout(p);
+    	}
+    	
+    	CardRow getCardRow(String s) {
+    		for(CardRow cr : cardRow.values())
+    			if(cr.name.equals(s) == true)
+    				return cr;
+    		return null;
+    	}
+    	
+    	public String toString() {
+    		String temp = "";
+    		
+    		for(CardRow cr : cardRow.values()) {
+    			temp += cr.name + "| ";
+    			for(int i = 0; i < playerCol.size(); i++)
+    				temp += cr.players.get(i).crossedout + " ";
+    			temp += "\n";
+    		}
+    		
+    		return temp;
+    	}
+    	
+    	
+    	class CardRow{
+    		String name;
+    		List<Position> players = new ArrayList<Position>();
+    		boolean isFound = false;
+    		int owner;
+    		
+     		CardRow(String name){
+    			this.name = name;
+    		}
+    		
+    		void crossout(int p){
+    			players.get(p).crossedout = true;
+    			
+    			int count = 0;
+    			for(Position q : players)
+    				if(q.crossedout == false) count++;
+    			if(count == 1) Found();
+    		}
+    		
+    		void crossoutAllBarOne(int p) {
+    			for(int i = 0; i < players.size();i++)
+    				if(i != p) crossout(i);
+    			Found();
+    		}
+    		
+    		void Found() {
+    			isFound = true;
+    			for(Position q : players)
+    				if(q.crossedout == false)
+    					owner = q.player;
+    		}
+    		
+    	}
+    	
+    	class PlayerColum{
+    		int index;    		
+    		List<Position> cards= new ArrayList<Position>();;
+    		
+    		PlayerColum(int index){
+    			this.index = index;
+    		}
+    	}
+    	
+    	class Position{
+        		int player;
+        		String card;
+        		
+        		public boolean crossedout = false;
+        		
+        		Position(int plyaer, String card){}
+        	}
+    }
+
+    class LogParser {
+    	
+    	java.util.Map<String,String> ParseResponce(String logMessage){
+		java.util.Map<String, String> map = new HashMap<String,String>();
+		String[] part = logMessage.split(" ");
+		
+		map.put("showingPlayer", part[0]);
+		map.put("showedCard", part[1].equals("showed") ? "true":"false" );
+		if(part[3].equals("card:"))
+			map.put("cardShowen", part[4]);
+		
+		return map;
+	}
+	
+		java.util.Map<String, String> ParseAnouncment(String logMessage){
+		java.util.Map<String, String> map = new HashMap<String,String>();
+		
+		String[] part = logMessage.split(" ");
+		
+		map.put("firstPlayer", part[0]);
+		map.put("secondPlayer", part[2]);
+		map.put("characterCard", part[4]);
+		map.put("weaponCard", part[7]);
+		map.put("roomCard", part[10].substring(0, part[10].length()-1));//removeing full stop
+		
+		return map;
+	}
+	}
+
+    class Question {
+    	CardMatrix myMatrix;
+    	Collection<String> cards;
+    	int asker;
+    	int ressponder;
+    	
+    	Question(Collection<String> cards) {
+    		this.cards = cards;
+    	}
+    }
 }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
